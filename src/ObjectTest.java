afd5457902d030e0219a936c04661f4febe8df05
Dayong Lee <50468628+dayongbz@users.noreply.github.com>
2020-08-20 17:43:56 +0900

    object method test

--- src/ObjectTest.java
+++ src/ObjectTest.java
@@ -1,13 +1,28 @@
-public class ObjectTest {
-    public static void main(String[] args) {
-        ObjectTest obj = new ObjectTest();
-        obj.checkToString();
+import java.util.Objects;
 
+public class ObjectTest {
+    int num = 0, age = 20;
+    String name = "dayong";
+
+    public static void main(String[] args) {
+        ObjectTest obj1 = new ObjectTest();
+        ObjectTest obj2 = new ObjectTest();
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
     }
 
-    public void checkToString() {
-        System.out.println(this);
-        System.out.println(toString());
-        System.out.println(this + "32");
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ObjectTest that = (ObjectTest) o;
+        return num == that.num &&
+                age == that.age &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num, age, name);
     }
 }
