d81aabc2dfb055d3aa091cfce5d5aa7dbec86f6b
Dayong Lee <50468628+dayongbz@users.noreply.github.com>
2020-09-21 15:41:26 +0900

    study enum

---
+++ src/c/model/MemberDTO.java
@@ -0,0 +1,7 @@
+package c.model;
+
+public class MemberDTO {
+    public String name;
+    public String phone;
+    public String email;
+}
---
+++ src/c/service/MemberManagerImp1.java
@@ -0,0 +1,10 @@
+package c.service;
+
+import c.model.MemberDTO;
+
+public class MemberManagerImp1 implements MemberManager {
+
+    public boolean addMember(MemberDTO member){return false;}
+    public boolean removeMember(String name, String phone){return false;}
+    public boolean updateMember(MemberDTO member){return false;}
+}
